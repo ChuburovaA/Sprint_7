a6cc424e037fd557729d4e9a534ae1bcabf08e72
Chuburova Alina <amminami@yandex.ru>
2024-05-06 13:34:14 +0300

    add class TestLoginCourier in  file test_login_courier.py

---
+++ tests/test_login_courier.py
@@ -0,0 +1,10 @@
+import pytest
+import allure
+import requests
+
+from data import URL, TextMessage
+from helpers import Helper
+from conftest import create_new_courier, login_and_delete_courier, register_new_courier
+
+@allure.story("Проверки на логин курьера")
+class TestLoginCourier:
