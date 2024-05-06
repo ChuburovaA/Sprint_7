4e7eec9378fee69bba167354d11f6584b0123d57
Chuburova Alina <amminami@yandex.ru>
2024-05-06 13:35:08 +0300

    add test_successful_login_courier in file test_login_courier.py

--- tests/test_login_courier.py
+++ tests/test_login_courier.py
@@ -8,3 +8,10 @@ from conftest import create_new_courier, login_and_delete_courier, register_new_
 
 @allure.story("Проверки на логин курьера")
 class TestLoginCourier:
+
+    @allure.title("Проверка наличия id при успешном логине курьера")
+    def test_successful_login_courier(self, login_and_delete_courier):
+        response = login_and_delete_courier
+        response_body = response.json()
+
+        assert response.status_code == 200 and 'id' in response_body.keys()
