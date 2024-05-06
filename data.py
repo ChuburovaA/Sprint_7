8ff5389a46d717c9aab08abd8453ee91f2081e9d
Chuburova Alina <amminami@yandex.ru>
2024-05-06 13:14:01 +0300

    add class TextMessage in file data.py

--- data.py
+++ data.py
@@ -6,3 +6,10 @@ class URL:
     url_get_order_list = 'https://qa-scooter.praktikum-services.ru/api/v1/orders?courierId='
     url_accept_order = 'https://qa-scooter.praktikum-services.ru/api/v1/orders/accept'
     url_get_order = 'https://qa-scooter.praktikum-services.ru/api/v1/orders/track'
+
+class TextMessage:
+    account_not_found = 'Учетная запись не найдена'
+    no_login_information = "Недостаточно данных для входа"
+    no_information_to_account = 'Недостаточно данных для создания учетной записи'
+    used_login = 'Этот логин уже используется. Попробуйте другой.'
+    successful_registration = '{"ok":true}'
