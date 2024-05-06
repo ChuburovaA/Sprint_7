aa3d2a3ed0d51261f7f9db4a21350b5f2cec6b5c
Chuburova Alina <amminami@yandex.ru>
2024-05-06 13:26:33 +0300

    modification file data

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 class URL:
-
     url_login_courier = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login'
     url_create_courier = 'https://qa-scooter.praktikum-services.ru/api/v1/courier'
+    url_delete_courier = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/'
     url_create_order = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
     url_get_order_list = 'https://qa-scooter.praktikum-services.ru/api/v1/orders?courierId='
     url_accept_order = 'https://qa-scooter.praktikum-services.ru/api/v1/orders/accept'
