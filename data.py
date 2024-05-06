fac070af1188ea4a67954838fb3e02cd14f9308e
Chuburova Alina <amminami@yandex.ru>
2024-05-06 13:13:24 +0300

    add class URL in file data.py

---
+++ data.py
@@ -0,0 +1,8 @@
+class URL:
+
+    url_login_courier = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login'
+    url_create_courier = 'https://qa-scooter.praktikum-services.ru/api/v1/courier'
+    url_create_order = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
+    url_get_order_list = 'https://qa-scooter.praktikum-services.ru/api/v1/orders?courierId='
+    url_accept_order = 'https://qa-scooter.praktikum-services.ru/api/v1/orders/accept'
+    url_get_order = 'https://qa-scooter.praktikum-services.ru/api/v1/orders/track'
