a97897d9d09f0c7869337eb6f568428960698e95
Chuburova Alina <amminami@yandex.ru>
2024-05-06 13:43:32 +0300

    add test_add_color_in_order in file test_order.py

--- tests/test_order.py
+++ tests/test_order.py
@@ -8,3 +8,14 @@ from conftest import create_new_courier, login_and_delete_courier, register_new_
 
 @allure.story("Проверки на создание заказа и получение списка")
 class TestOrder:
+
+    @allure.title("Проверка получения успешного заказа при выборе цветов самоката из списка")
+    @pytest.mark.parametrize('color', OrderData.color_list)
+    def test_add_color_in_order(self, color):
+        payload = OrderData.order_data
+        payload['color'] = color
+        payload = json.dumps(OrderData.order_data)
+        response = requests.post(URL.url_create_order, data=payload)
+        response_body = response.json()
+
+        assert response.status_code == 201 and 'track' in response_body.keys()
