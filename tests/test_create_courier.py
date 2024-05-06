96ecf8dd9215997d79392d1bff6c3ff42413cfcf
Chuburova Alina <amminami@yandex.ru>
2024-05-06 13:23:27 +0300

    add test_registration_courier_successful in file test_create_courier.py

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -6,4 +6,10 @@ from data import URL, TextMessage
 from helpers import Helper
 from conftest import create_new_courier, register_new_courier, login_and_delete_courier
 
+@allure.story("Проверки на создание курьера")
 class TestCreateCourier:
+    @allure.title("Проверка успешного создания курьера")
+    def test_registration_courier_successful(self, register_new_courier, login_and_delete_courier):
+        response = register_new_courier
+
+        assert response.status_code == 201 and response.text == TextMessage.successful_registration
