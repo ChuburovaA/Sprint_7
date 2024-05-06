943f4f35365a9b96d997cf2f55da7fa306b08162
Chuburova Alina <amminami@yandex.ru>
2024-05-06 13:16:55 +0300

    add fixture create_new_courier in file conftest.py

--- conftest.py
+++ conftest.py
@@ -4,3 +4,16 @@ import pytest
 from data import URL
 from helpers import Helper
 
+@pytest.fixture
+def create_new_courier():
+    login = Helper.generate_random_string(10)
+    password = Helper.generate_random_string(10)
+    first_name = Helper.generate_random_string(10)
+
+    payload = {
+        "login": login,
+        "password": password,
+        "firstName": first_name
+    }
+
+    return payload
